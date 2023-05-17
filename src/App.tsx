b8fa4abcd1c476f2624f74cedacba8e0a5fe4651
VLadislav Samukhin <vsamukhin@gmail.com>
2023-05-17 23:42:35 +0600

    add function

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Circle from "./Circle/Circle";
 import './App.css';
 
-const App = () => (
-    <div className="App">
+const App = () => {
+    const [numbers , setNumbers] = useState<number[]>([]);
 
-    </div>
-);
+    const getRandomNumbers = (min:number,max:number) => {
+        let num:number[] = [];
+        while (num.length !== 5){
+            const number:number = Math.floor(min + Math.random() * (max - min));
+            if (!num.includes(number)){
+                num.push(number);
+            }
+        }
+        return num;
+    };
+    const changeNumbers = () => {
+        let numberArray:number[] = getRandomNumbers(5,36);
+        numberArray.sort((a:number,b:number) => a-b);
+        setNumbers(numberArray);
+    };
+
+    return(
+        <div className="App">
+            <div className="container">
+                {numbers.map((number:number)=> (
+                    <Circle key={number} num={number}/>
+                ))}
+                <button onClick={changeNumbers}  type="button">New numbers</button>
+            </div>
+        </div>
+    );
+};
 
 export default App;
